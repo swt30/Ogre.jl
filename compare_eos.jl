8293e820a3af5523d83ed715be26b4078cc5b47f
Scott Thomas <swt30@ast.cam.ac.uk>
2015-08-17 14:03:05 +0100

    Add Seager graph values to the EOS comparison

--- compare_eos.jl
+++ compare_eos.jl
@@ -1,9 +1,20 @@
 using Ogre, PyPlot
 
+type GraphValues
+    P
+    ρ
+end
+function GraphValues(filename::String)
+    graph = readcsv(filename)
+    P = graph[:, 1]
+    ρ = graph[:, 2]
+    GraphValues(P, ρ)
+end
+
 function main()
-    graph_h2o    = readcsv("data/seager-graphs/H2O.csv")
-    graph_mgsio3 = readcsv("data/seager-graphs/MgSiO3.csv")
-    graph_fe     = readcsv("data/seager-graphs/Fe.csv")
+    graph_h2o    = GraphValues("data/eos/graphs/seager/h2o.csv")
+    graph_mgsio3 = GraphValues("data/eos/graphs/seager/mgsio3.csv")
+    graph_fe     = GraphValues("data/eos/graphs/seager/fe.csv")
 
     pressures    = logspace(5, 14)
 
@@ -23,15 +34,18 @@ function main()
     plot(pressures, my_h2o_500, label="(500K)")
     plot(pressures, my_h2o_800, label="(800K)")
     plot(pressures, my_h2o_1200, label="(1200K)")
+    plot(graph_h2o.P, graph_h2o.ρ, label="Seager H2O")
+    plot(graph_mgsio3.P, graph_mgsio3.ρ, label="Seager MgSiO3")
+    plot(graph_fe.P, graph_fe.ρ, label="Seager Fe")
 
     xlabel("Pressure (P) / Pa")
     ylabel(L"Density ($\rho$) / kg m$^{-3}$")
     xlim(1e5, 1e14)
-    ylim(1e1, 1e5)
+    # ylim(1e1, 1e5)
     xscale("log")
     yscale("log")
 
-    legend(loc=0)
+    legend(loc="best")
     tight_layout()
 end
 
